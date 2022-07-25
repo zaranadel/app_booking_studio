21fb66b96b19f216598e9b457953e1b53eb461d7
TeddyBHidayat <biantoro.teddyyh@gmail.com>
2022-07-25 16:06:59 +0700

    asdadsasdads

--- resources/views/ruangstudio_form.blade.php
+++ resources/views/ruangstudio_form.blade.php
@@ -6,7 +6,7 @@
     <!-- Main content -->
     <section class="content">
             <div class="card">
-                <div class="card-header">Tambah Ruang Studio</div>
+                <div class="card-header bg-dark">Ruang Studio</div>
 
                 <div class="card-body">
                     {!! Form::model($model, ['route' => $route, 'method' => $method,'files' => true]) !!}
